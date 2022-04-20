385daa3e34c0a33b5ba032a556b24f1072ae45e9
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2022-04-20 08:26:46 -0700

    Update BiosUpdateInstall.ps1

--- BiosUpdateInstall.ps1
+++ BiosUpdateInstall.ps1
@@ -1,2 +1,2 @@
-Invoke-WebRequest 'https://dl.dell.com/FOLDER08358542M/1/OptiPlex_5060_1.18.0.exe' -Outfile 'C:\Windows\temp\OptiPlex_5060_1.18.0.exe'
-Start-Process -FilePath "C:\Windows\temp\OptiPlex_5060_1.18.0.exe" $arg -WorkingDirectory "C:\Windows\temp"
+Invoke-WebRequest -Uri https://faronics.digitalpigeon.com/msg/c7kXEMC7EeykMgYt773E8Q/tiFrrnfA7GdGLKuWm2AORQ/file/73e249f0-c0bb-11ec-b358-06d092decc8d/download -OutFile c:\windows\temp\DellBiosUpdate.exe
+Start-Process -FilePath "c:\windows\temp\DellBiosUpdate.exe" $arg -WorkingDirectory "C:\Windows\temp"
